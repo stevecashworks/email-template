2aede149aebd1fd1d77e6d98db6dbca3b0693638
stevecashworks <chigbustephennamdi@gmail.com>
2023-09-10 22:51:17 +0100

    template-added

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
- const  express=require('express');
+const html= require("./html.js")
+const  express=require('express');
  const nodemailer=require('nodemailer');
+ const newEmail="tradesavorinfo@gmail.com"
+  const EmailPassword= "iytoyydtgdmkijll"
 
  const Router= express.Router();
  Router.route('/').post(
@@ -8,15 +11,15 @@
        const transporter=nodemailer.createTransport({
           service:'gmail',
           auth:{
-             user:'eventsnorwood@gmail.com',
-              pass:'ldxybwgafkzoijwn',
+             user:/*'eventsnorwood@gmail.com'*/newEmail,
+              pass:/*'ldxybwgafkzoijwn'*/EmailPassword,
 
           }})
           const options={
              from:'eventsnorwood@gmail.com',
              to:email,
-             subject:"Norwood events",
-             text:message
+             subject:"TradeSavor",
+             html:html
     
           }
           transporter.sendMail(options,(err,info)=>{
